6c84516e88b8a1001e7714da9da8eec2043c3f4c
Jason Song <jiachens@andrew.cmu.edu>
2016-05-09 18:48:51 -0400

    Remove .o.d

--- app/src/main/obj/local/armeabi-v7a/objs/volume/vecpy_volume_kernel.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi-v7a/objs/volume/vecpy_volume_kernel.o: \
- jni/vecpy_volume_kernel.c jni/vecpy_volume_kernel.h \
- /Users/Jason/Documents/AndroidDev/android-sdk-macosx/ndk-bundle/sources/cxx-stl/stlport/stlport/math.h
-
-jni/vecpy_volume_kernel.h:
-
-/Users/Jason/Documents/AndroidDev/android-sdk-macosx/ndk-bundle/sources/cxx-stl/stlport/stlport/math.h:
